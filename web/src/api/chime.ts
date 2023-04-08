64bcd55c06cfa12e57417a0d461d5ff5e304fc6f
Roberson Andrade <roberson_souza11@hotmail.com>
2023-04-08 12:01:40 -0300

    feat(web): Add create channel request

---
+++ web/src/api/chime.ts
@@ -0,0 +1,38 @@
+import {
+  ChimeSDKMessagingClient,
+  CreateChannelCommand,
+} from '@aws-sdk/client-chime-sdk-messaging';
+import { getAppInstanceArn, generateUserArn } from '../pages/chat/utils';
+
+interface CreateChannelProperties {
+  chimeClient: ChimeSDKMessagingClient;
+  currentUserId: string;
+  userToChatId: string;
+}
+
+export async function createChannel({
+  chimeClient,
+  currentUserId,
+  userToChatId,
+}: CreateChannelProperties) {
+  const appInstanceArn = getAppInstanceArn();
+
+  try {
+    const channelResponse = await chimeClient.send(
+      new CreateChannelCommand({
+        AppInstanceArn: appInstanceArn,
+        ChimeBearer: generateUserArn(currentUserId),
+        Name: `${currentUserId}#${userToChatId}`,
+      })
+    );
+
+    if (!channelResponse.ChannelArn) {
+      throw new Error('Error');
+    }
+
+    return channelResponse.ChannelArn;
+  } catch (error) {
+    console.log('error', error);
+    return 'Error while creating chat channel';
+  }
+}
