f23234fa46b9abe777372263e26493429af766a0
Roberson Andrade <roberson_souza11@hotmail.com>
2023-04-10 19:40:42 -0300

    refactor(scripts): Add comments to the chime script

--- scripts/chime.sh
+++ scripts/chime.sh
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-APP_INSTANCE_ARN=""
-APP_INSTANCE_USER_ARN=""
+APP_INSTANCE=""
+USER_ARN=""
 APP_INSTANCE_NAME="aws-chime-demo"
 
-APP_INSTANCE_ARN=$(aws chime list-app-instances --query "AppInstances[*].{ name:Name, appInstanceArn:AppInstanceArn }[?name=='$APP_INSTANCE_NAME'].appInstanceArn" --output text)
+# Verify if the app instance already exists
+APP_INSTANCE=$(aws chime list-app-instances --query "AppInstances[*].{ name:Name, appInstanceArn:AppInstanceArn }[?name=='$APP_INSTANCE_NAME'].appInstanceArn" --output text)
 
-if [ -n "$APP_INSTANCE_ARN" ]; then
-	echo "$APP_INSTANCE_ARN"
+# Print the app instance if exists
+if [ -n "$APP_INSTANCE" ]; then
+	echo "$APP_INSTANCE"
 	exit
 fi
 
-APP_INSTANCE_ARN=$(aws chime create-app-instance --name "$APP_INSTANCE_NAME" --region us-east-1 --output text)
+# Create an app instance
+APP_INSTANCE=$(aws chime create-app-instance --name "$APP_INSTANCE_NAME" --region us-east-1 --output text)
 
-APP_INSTANCE_USER_ARN=$(aws chime create-app-instance-user \
-	--app-instance-arn "$APP_INSTANCE_ARN" \
-	--app-instance-user-id "application-worker" \
-	--name "application-worker" \
-	--region us-east-1 \
-	--output text)
+# Create an user to be used by our application using the app instance
+USER_ARN=$(aws chime create-app-instance-user --app-instance-arn "$APP_INSTANCE" --app-instance-user-id "application-worker" --name "application-worker" --region us-east-1 --output text)
 
-aws chime-sdk-identity create-app-instance-admin \
-	--app-instance-arn "$APP_INSTANCE_ARN" \
-	--app-instance-admin-arn "$APP_INSTANCE_USER_ARN" \
-	--region us-east-1 \
-	--output text >/dev/null
+# Promote the user to an admin
+aws chime-sdk-identity create-app-instance-admin --app-instance-arn "$APP_INSTANCE" --app-instance-admin-arn "$USER_ARN" --region us-east-1 --output text >/dev/null
 
-echo "$APP_INSTANCE_ARN"
+echo "$APP_INSTANCE"
